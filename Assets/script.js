50a0de514f13bed2c01f8c8235ab24cbd9a04d3e
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-05 14:35:23 -0600

    started apikey

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,25 @@
-console.log("something");
+let apiKey = "7cfb14f7bc269e860ff7c1a3b2f56ce0";
+
+let searchButton = document.getElementById('search-button')
+
+let city=[];
+
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+
+
+
+
+
+
+// searchButton.addEventListener('click', function(event) {
+// let userCityInput=event.target;
+// console.log(userCityInput);
+// })
+
+searchButton.addEventListener('click', function(event) {
+    city=event.target;
+    console.log(city);
+    })
+    console.log(city);
+
