303f00d18f754eca1638785b65ca245b3b3b69e0
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-05 17:13:00 -0600

    updates

--- Assets/script.js
+++ Assets/script.js
@@ -2,24 +2,42 @@ let apiKey = "7cfb14f7bc269e860ff7c1a3b2f56ce0";
 
 let searchButton = document.getElementById('search-button')
 
+// console.log(searchButton);
+
 let city=[];
+// console.log(city);
 
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-
-
+let baseURL = "https://api.openweathermap.org/"
 
 
-
-
-// searchButton.addEventListener('click', function(event) {
-// let userCityInput=event.target;
-// console.log(userCityInput);
-// })
 
 searchButton.addEventListener('click', function(event) {
-    city=event.target;
-    console.log(city);
+    let city=event.target;
+    // console.log(city);
+
+    let chicagoLongLat = "data/2.5/weather?lat=41.9019&lon=-87.6779&appid=7cfb14f7bc269e860ff7c1a3b2f56ce0"
+
+    let chicagoURL = "geo/1.0/zip?zip=60622,US&appid=7cfb14f7bc269e860ff7c1a3b2f56ce0"
+
+   let queryURL = "data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+    //fetch request 1
+
+  fetch(baseURL + queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
     })
-    console.log(city);
+
+//   fetch(baseURL + chicagoLongLat)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data)
+//     })
+})
+   
 
