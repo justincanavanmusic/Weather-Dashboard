94c186a105d397b9ad2c8ff3df60643c8d2278b3
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-05 19:21:05 -0600

    functioning search bar with API's, info displays proper in big white box

--- Assets/script.js
+++ Assets/script.js
@@ -1,43 +1,64 @@
 let apiKey = "7cfb14f7bc269e860ff7c1a3b2f56ce0";
 
 let searchButton = document.getElementById('search-button')
-
+let userTextInput = document.getElementById('exampleFormControlInput1')
 // console.log(searchButton);
-
-let city=[];
-// console.log(city);
 
 let baseURL = "https://api.openweathermap.org/"
 
 
-
 searchButton.addEventListener('click', function(event) {
-    let city=event.target;
+   let city=userTextInput.value;
     // console.log(city);
 
-    let chicagoLongLat = "data/2.5/weather?lat=41.9019&lon=-87.6779&appid=7cfb14f7bc269e860ff7c1a3b2f56ce0"
-
-    let chicagoURL = "geo/1.0/zip?zip=60622,US&appid=7cfb14f7bc269e860ff7c1a3b2f56ce0"
-
-   let queryURL = "data/2.5/weather?q=" + city + "&appid=" + apiKey;
+   let queryURL = baseURL + "data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
     //fetch request 1
 
-  fetch(baseURL + queryURL)
+  fetch(queryURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data)
-    })
 
-//   fetch(baseURL + chicagoLongLat)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data)
-//     })
-})
+    console.log(data);
+
+    let windSpeed=(data.wind.speed);
+    console.log(windSpeed);
+    
+    let humidity=(data.main.humidity);
+    console.log(humidity);
+    
+    let temp=(data.main.temp);
+    console.log(temp);
+
+    let icon=data.weather[0].icon;
+    console.log(icon);
+
+    // todays date
+    
+    let today=new Date();
+
+    let dayOfYear=today.getDate();
    
+    let monthOfYear=today.getMonth()+1; 
+    let year=today.getFullYear(); 
+    
+    let todaysDate=(`${monthOfYear}/${dayOfYear}/${year}`);
+   
+    document.getElementById("big-white-header").textContent=`${city} ${todaysDate} ${icon}`;
+
+    document.getElementById("big-white-temp").textContent="Temp: " + `${temp}`;
+
+    document.getElementById("big-white-wind").textContent="Wind: " + `${windSpeed}` + " MPH";
+
+    document.getElementById("big-white-humidity").textContent="Humidity: " + `${humidity}` + " %";
+
+    })
+})
+
+
+
+
+
 
